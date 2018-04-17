d1bfc61735116f856d14de96d3144e636f341b5f
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-04-17 10:58:39 +0200

    add coding header

--- cs/pfg/mipago/interfaces/__init__.py
+++ cs/pfg/mipago/interfaces/__init__.py
@@ -2,7 +2,13 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface
 
 
 class ICS_PFG_MIPAGO(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class IMiPagoAdapter(Interface):
+    pass
+
+# -*- extra stuff goes here -*-
