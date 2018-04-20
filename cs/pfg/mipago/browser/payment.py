913000d6215bf1ebebed64c32803c368a3b72e3a
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-04-20 11:44:25 +0200

    show information about the form data in the thanks form

--- cs/pfg/mipago/browser/payment.py
+++ cs/pfg/mipago/browser/payment.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
-from Products.Five.browser import BrowserView
 from Acquisition import aq_parent
+from cs.pfg.mipago.config import ANNOTATION_KEY
+from cs.pfg.mipago.config import MIPAGO_HTML_KEY
+from cs.pfg.mipago.config import MIPAGO_PAYMENT_CODE
+from Products.Five.browser import BrowserView
+from zope.annotation import IAnnotations
 
 
 class PaymentEnd(BrowserView):
 
-    def __call__(self):
-        parent = aq_parent(self.context)
-        return self.request.response.redirect(self.context.absolute_url() + '/fg_result_view')
+    def get_payment_data(self):
+        adapted = IAnnotations(self.context)
+        payment_code = self.request.SESSION.get(MIPAGO_PAYMENT_CODE, None)
+        if payment_code is not None:
+            payments = adapted.get(ANNOTATION_KEY, {})
+            return payments.get(payment_code, None)
+
+        return None
 
