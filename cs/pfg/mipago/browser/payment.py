6644fc97eaff003804a31f2c157c40132d5dc267
Mikel Larreategi <mlarreategi@codesyntax.com>
2018-04-20 12:23:17 +0200

    rework payment end template

--- cs/pfg/mipago/browser/payment.py
+++ cs/pfg/mipago/browser/payment.py
@@ -3,8 +3,11 @@ from Acquisition import aq_parent
 from cs.pfg.mipago.config import ANNOTATION_KEY
 from cs.pfg.mipago.config import MIPAGO_HTML_KEY
 from cs.pfg.mipago.config import MIPAGO_PAYMENT_CODE
+from DateTime import DateTime
+from Products.CMFPlone.utils import safe_hasattr
 from Products.Five.browser import BrowserView
 from zope.annotation import IAnnotations
+from zope.component import getMultiAdapter
 
 
 class PaymentEnd(BrowserView):
@@ -18,3 +21,42 @@ class PaymentEnd(BrowserView):
 
         return None
 
+    @property
+    def form(self):
+        return aq_parent(self.context)
+
+    def get_thanks_page(self):
+        if safe_hasattr(self.form, 'thanksPageOverride'):
+            s = self.form.getThanksPageOverride()
+            if s:
+                return None
+
+        s = getattr(self.form, 'thanksPage', None)
+        if s:
+            return getattr(self.form, s, None)
+
+        return None
+
+    def form_title(self):
+        thanks = self.get_thanks_page()
+        if thanks is not None:
+            return thanks.Title()
+
+        return self.form.Title()
+
+    def get_thanks_prologue(self):
+        thanks = self.get_thanks_page()
+        if thanks is not None:
+            return thanks.getThanksPrologue()
+        return ''
+
+    def get_thanks_epilogue(self):
+        thanks = self.get_thanks_page()
+        if thanks is not None:
+            return thanks.getThanksEpilogue()
+        return ''
+
+    def datetime(self):
+        now = DateTime()
+        ploneview = getMultiAdapter((self.context, self.request), name='plone')
+        return ploneview.toLocalizedTime(now, long_format=True)
