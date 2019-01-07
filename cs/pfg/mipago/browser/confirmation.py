5684df89a8a58ac0d071e6ae792c7d0c825d5ed4
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-01-07 08:59:01 +0100

    send confirmation email after receiving the payment confirmation notification

--- cs/pfg/mipago/browser/confirmation.py
+++ cs/pfg/mipago/browser/confirmation.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_parent
 from cs.pfg.mipago.config import ANNOTATION_KEY
+from cs.pfg.mipago.config import MIPAGO_HTML_KEY
+from cs.pfg.mipago.config import MIPAGO_PAYMENT_CODE
 from cs.pfg.mipago.config import PAYMENT_STATUS_ERROR_IN_MIPAGO
 from cs.pfg.mipago.config import PAYMENT_STATUS_PAYED
 from cs.pfg.mipago.config import PAYMENT_STATUS_SENT_TO_MIPAGO
@@ -15,21 +17,22 @@ import xml.etree.ElementTree as ET
 
 
 class PaymentConfirmation(BrowserView):
-
     def get_annotation_context(self):
         parent = aq_parent(self.context)
-        if safe_hasattr(parent, 'isTranslation') and \
-               parent.isTranslation() and not parent.isCanonical():
+        if (
+            safe_hasattr(parent, "isTranslation")
+            and parent.isTranslation()
+            and not parent.isCanonical()
+        ):
             # look in the canonical version to see if there is
             # a matching (by id) save-data adapter.
             # If so, call its onSuccess method
             cf = parent.getCanonical()
             target = cf.get(self.context.getId())
-            if target is not None and target.meta_type == 'MiPagoAdapter':
+            if target is not None and target.meta_type == "MiPagoAdapter":
                 return target
 
         return self.context
-
 
     def __call__(self):
         alsoProvides(self.request, IDisableCSRFProtection)
@@ -39,39 +42,76 @@ class PaymentConfirmation(BrowserView):
         payment_status = self.extract_payment_status()
         payments = adapted.get(ANNOTATION_KEY, {})
         payment_data = payments.get(payment_code, {})
-        payment_data['status'] = payment_status
+        payment_data["status"] = payment_status
         payments[payment_code] = payment_data
         adapted[ANNOTATION_KEY] = payments
+        if payment_status == PAYMENT_STATUS_PAYED:
+            self.send_form(payment_code)
+
         return 1
 
     def extract_payment_code(self):
         # inspect self.request
         from logging import getLogger
+
         log = getLogger(__name__)
-        log.info('####### extract_payment_code ###########')
+        log.info("####### extract_payment_code ###########")
         log.info(self.request.items())
-        log.info('####### /extract_payment_code ###########')
+        log.info("####### /extract_payment_code ###########")
 
-        param = self.request.get('param1', '')
+        param = self.request.get("param1", "")
         return self._parse_param(param)
-
 
     def _parse_param(self, param):
         root = ET.fromstring(param)
-        id_item = root.find('.//id')
+        id_item = root.find(".//id")
         if id_item is not None:
             return id_item.text
 
-        return ''
+        return ""
 
     def extract_payment_status(self):
         # inspect self.request
-        function = self.request.get('function', '')
-        if function == 'onBeginPayment':
+        function = self.request.get("function", "")
+        if function == "onBeginPayment":
             return PAYMENT_STATUS_USER_IN_MIPAGO
-        elif function == 'onPayONLineOK':
+        elif function == "onPayONLineOK":
             return PAYMENT_STATUS_PAYED
-        elif function == 'onPayONLineNOK':
+        elif function == "onPayONLineNOK":
             return PAYMENT_STATUS_ERROR_IN_MIPAGO
 
-        return ''
+        return ""
+
+    def get_payment_data(self, payment_code=None):
+        annotation_context = self.get_annotation_context()
+        adapted = IAnnotations(annotation_context)
+
+        if payment_code is not None:
+            payments = adapted.get(ANNOTATION_KEY, {})
+            return payments.get(payment_code, None)
+
+        return None
+
+    def send_form(self, payment_code):
+        data = self.get_payment_data(payment_code)
+        if data is not None:
+            field_data = data.get("fields", [])
+            fields = []
+            form_data = {}
+            for field in field_data:
+                form_data[field["id"]] = field["value"]
+                fields.append(self.form.get(field["id"]))
+
+            self.request.form = form_data
+
+            # Mail to form owner
+            self.context.send_form(
+                fields, self.request, to_addr=self.context.getRecipient_email()
+            )
+
+            to_field = self.context.getTo_field()
+            if to_field != "#NONE#":
+                # Mail to form filler
+                self.context.send_form(
+                    fields, self.request, to_addr=form_data[to_field]
+                )
--- cs/pfg/mipago/browser/payment.py
+++ cs/pfg/mipago/browser/payment.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from Acquisition import aq_parent
 from cs.pfg.mipago.config import ANNOTATION_KEY
-from cs.pfg.mipago.config import MIPAGO_HTML_KEY
 from cs.pfg.mipago.config import MIPAGO_PAYMENT_CODE
 from DateTime import DateTime
 from Products.CMFPlone.utils import safe_hasattr
@@ -11,7 +10,6 @@ from zope.component import getMultiAdapter
 
 
 class PaymentEnd(BrowserView):
-
     def get_payment_data(self):
         adapted = IAnnotations(self.context)
         payment_code = self.request.SESSION.get(MIPAGO_PAYMENT_CODE, None)
@@ -26,12 +24,12 @@ class PaymentEnd(BrowserView):
         return aq_parent(self.context)
 
     def get_thanks_page(self):
-        if safe_hasattr(self.form, 'thanksPageOverride'):
+        if safe_hasattr(self.form, "thanksPageOverride"):
             s = self.form.getThanksPageOverride()
             if s:
                 return None
 
-        s = getattr(self.form, 'thanksPage', None)
+        s = getattr(self.form, "thanksPage", None)
         if s:
             return getattr(self.form, s, None)
 
@@ -48,39 +46,42 @@ class PaymentEnd(BrowserView):
         thanks = self.get_thanks_page()
         if thanks is not None:
             return thanks.getThanksPrologue()
-        return ''
+        return ""
 
     def get_thanks_epilogue(self):
         thanks = self.get_thanks_page()
         if thanks is not None:
             return thanks.getThanksEpilogue()
-        return ''
+        return ""
 
     def datetime(self):
         now = DateTime()
-        ploneview = getMultiAdapter((self.context, self.request), name='plone')
+        ploneview = getMultiAdapter((self.context, self.request), name="plone")
         return ploneview.toLocalizedTime(now, long_format=True)
-
 
     def send_form(self):
         data = self.get_payment_data()
         if data is not None:
-            field_data = data.get('fields', [])
+            field_data = data.get("fields", [])
             fields = []
             form_data = {}
             for field in field_data:
-                form_data[field['id']] = field['value']
-                fields.append(self.form.get(field['id']))
+                form_data[field["id"]] = field["value"]
+                fields.append(self.form.get(field["id"]))
 
             self.request.form = form_data
 
             # Mail to form owner
-            self.context.send_form(fields, self.request, to_addr=self.context.getRecipient_email())
+            self.context.send_form(
+                fields, self.request, to_addr=self.context.getRecipient_email()
+            )
 
             to_field = self.context.getTo_field()
-            if to_field != '#NONE#':
+            if to_field != "#NONE#":
                 # Mail to form filler
-                self.context.send_form(fields, self.request, to_addr=form_data[to_field])
+                self.context.send_form(
+                    fields, self.request, to_addr=form_data[to_field]
+                )
 
     def clear_session(self):
         for key in self.request.SESSION.keys():
