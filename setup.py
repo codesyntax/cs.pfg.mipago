0172f3d32d73a016089ed71f2ba5e5db29aeaede
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-01-07 09:01:36 +0100

    Back to development: 1.0b17
    [ci skip]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0b16'
+version = '1.0b17.dev0'
 
 long_description = (
     open('README.rst').read() + '\n' +
